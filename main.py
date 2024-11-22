f3fc0af2aea47119b6aa5d7297c818e36bba0135
Alex Bramartha <91941480+alexbramartha1@users.noreply.github.com>
2024-11-22 11:31:27 +0800

    Update main

--- main.py
+++ main.py
@@ -81,11 +81,11 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-origins = ['http://localhost:3000']
+# origins = ['http://localhost:3000']
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
+    allow_origins = ["*"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
